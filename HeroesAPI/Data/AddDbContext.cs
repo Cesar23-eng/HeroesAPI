f63fcc29e0343c0ff88365e57d2d40fbb3431d89
Cesar23-eng <a2023114080@estudiantes.upsa.edu.bo>
2025-03-17 20:12:51 -0400

    ahora si xd

--- HeroesAPI/Data/AddDbContext.cs
+++ HeroesAPI/Data/AddDbContext.cs
@@ -1,6 +1,9 @@
-﻿namespace HeroesAPI.Data;
+﻿using HeroesAPI.Models;
+using Microsoft.EntityFrameworkCore;
 
-public class AddDbContext
+namespace HeroesAPI.Data;
+
+public class AppDbContext(DbContextOptions<AppDbContext> options) : DbContext(options)
 {
-    
+    public DbSet<Hero> Heroes { get; set; }
 }
--- HeroesAPI/Models/Hero.cs
+++ HeroesAPI/Models/Hero.cs
@@ -2,5 +2,10 @@
 
 public class Hero
 {
-    
+    public int Id { get; set; }
+    public string FirstName { get; set; } = null!;
+    public string LastName { get; set; } = null!;
+    public string HeroName { get; set; } = null!;
+    public string City { get; set; } = null!;
+    public int Age { get; set; }
 }
